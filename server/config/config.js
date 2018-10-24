c1e75f724cc4b42b21253db47ee9a3370d4a9d01
Alexander <nubreed004@gmail.com>
2018-10-24 18:13:24 +0300

    Settin' up server

---
+++ server/config/config.js
@@ -0,0 +1,15 @@
+const config = {
+  production: {
+    SECRET: process.env.SECRET,
+    DATABASE: process.env.MONGO_URI
+  },
+  default: {
+    SECRET: 'supersecretpassword123',
+    DATABASE: 'mongodb://localhost:27017/booksShelf'
+  }
+}
+
+
+exports.get = function get(env) {
+  return config[env] || config.default
+}
--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+const config = require('./config/config').get(process.env.NODE_ENV);
+
+mongoose.Promise = global.Promise;
+mongoose.connect()
 
 const app = express();
 
